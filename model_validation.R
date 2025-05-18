500035703d35f120070381bfe27d3891afb4b5ac
Jonas Zhou <jonaszhou@buaa.edu.cn>
2025-05-18 17:06:39 +0800

    Update model_validation.R

--- model_validation.R
+++ model_validation.R
@@ -24,8 +24,7 @@ library(pdp)
 #####################################################################
 #####                    data info (necessary)                  #####
 #####################################################################
-label1<-excel_sheets('~/Desktop/Genome/rf-hpo-op_Genome.xlsx')
-#label2<-excel_sheets('~/Desktop/TBRFA/yfuber-TBRFA-d77db1a/burden/all-rf-op.xlsx')
+label1<-excel_sheets('/rf-hpo-op_Genome.xlsx')
 label<-c(label1)
 colindex<-c('MW','diameter','surface','purity','size','zp','shape', 'coating','media',
             'dissolution','illumination','duration','Nmetal','Moxygen','Xox','x','sigma_x','sigma_xnO',
@@ -44,27 +43,23 @@ colindex<-c('MW','diameter','surface','purity','size','zp','shape', 'coating','m
             'metabolite_interconversion_enzyme','protein_modifying_enzyme','chromatin_chromatin_binding_or_regulatory_protein','transfer_carrier_protein',
             'membrane_traffic_protein','chaperone','structural_protein','storage_protein','transmembrane_signal_receptor',
             
-            'Toxicity_Conc')   #设置顺序
+            'Toxicity_Conc')  
 
 #####################################################################
 #####                    Scatter output                         #####
 #####################################################################
 
-setwd('~/Desktop/Genome')
+setwd('~/')
 source(file = 'scatterplot_Genome.R',encoding = 'utf-8')
 
 scatterlist<-list()
 
   scatterlist[[label[1]]]<-reg(1,1)
 
-#scatterlist[[label2[1]]]<-reg(2,1,0,1)
-#scatterlist[[label2[2]]]<-reg(2,2,0,0.4)
-#scatterlist[[label2[3]]]<-reg(2,3,0,0.1)
-
 #####################################################################
 #####                    RF analysis                            #####
 #####################################################################
-setwd('~/Desktop/Genome')
+setwd('~/')
 rf.list<-list()
 
 data<-read.xlsx('Standard_Genome.xlsx',1,header=TRUE)
@@ -96,7 +91,7 @@ impframe<-data.frame(index=colindexf)
 
   print(1)
   rf<-rf.list[[1]]
-  imp<-importance(rf)#计算变量的重要性
+  imp<-importance(rf)
   incmse<-data.frame(index=names(imp[,2]),incmse=imp[,1]/max(imp[,1]))#incmse:increase in mean squared error
   colnames(incmse)[2]<-paste0(1)
   impframe<-merge(impframe,incmse,by='index',all=T)
@@ -108,91 +103,6 @@ impframe<-data.frame(index=colindexf)
   im_frame[4]<-im_frame[4]/max(im_frame[4])
   im_frame[5]<-im_frame[5]/max(im_frame[5])
   mi[[label[1]]]<-im_frame
-
-#colnames(impframe)[2:20]<-label
-save(impframe,md,mi,file='multi-importanceGenome.rda')
-
-edges<-data.frame()
-
-  edges[(1):(19),1]<-label[1]
-
-edges[2]<-NA
-
-for (i in 1:19){
-  edges[c(seq(i,1*19,19)),3]<-colindex[i]
-}
-
-for (j in 1:19){
-    edges[j,4]<-impframe[j,2]
-}
-
-edges[(1):(19),5]<-1
-
-colnames(edges)=c('from','to','label','mse','group')
-
-edges<-edges %>%
-  group_by(group) %>%
-  arrange(desc(mse), .by_group=TRUE)
-
-
-for (i in 1:nrow(edges)){
-  edges$to[i]<-paste0(edges$group[i],'_',edges$label[i])
-}
-
-edges_in1<-data.frame(from=c(''),to=c(''),
-                      label=c(''),mse=c(0),group=c(0))
-edges_in2<-data.frame(from=label1,to=label1,
-                      label=label1,mse=c(rep(0,1)),group=c(rep(0,1)))
-
-edges_all=rbind(edges_in2,edges)
-
-
-nodes<-data.frame(name=c(edges_all$to),
-                  label=c(edges_all$label),
-                  weight=c(as.numeric(edges_all$mse)),
-                  group=c(rep('B',20)))
-
-n_leaves<-nrow(nodes)-19+1
-nodes$id<-n_leaves/360*90
-nodes$id[19:nrow(nodes)]<-seq(1:n_leaves)
-
-nodes$angle<- 301.206-360 * nodes$id / n_leaves
-
-nodes$angle[1:20]<-0
-
-nodes$group[nodes$group==1]<-letters[1+3]
-
-
-mycolor1<-c('#000000','#B2DF8A','#FB9A99',
-            '#dc7bff','#a77bff','#7b84ff','#7bb8ff','#7beeff',
-            '#7bffdc','#7bffa7','#84ff7b','#b8ff7b','#eeff7b',
-            '#ffdc7b','#ffa77b','#ff7b84','#ff7bb8','#ff7bee')
-
-mycolor2<-c('#000000','#B2DF8A','#FB9A99',
-            '#c526ff','#6e26ff','#2635ff','#268bff','#26e2ff',
-            '#26ffc5','#26ff6e','#35ff26','#8bff26','#e2ff26',
-            '#ffc526','#ff6e26','#ff2635','#ff268b','#ff26e2')
-
-mygraph <- graph_from_data_frame( edges_all, vertices = nodes, directed = TRUE )
-
-ggraph(mygraph, layout = 'dendrogram', circular = TRUE) +
-  geom_edge_diagonal(aes(colour=..index..)) +
-  scale_edge_colour_distiller(palette = "RdPu") +
-  geom_node_text(aes(x = x*1.25, y=y*1.25, angle = angle,label=label,color=group),
-                 size=2, alpha=1) +
-  geom_node_point(aes( x = x*1.07, y=y*1.07, fill=group, size=weight),
-                  shape=21,stroke=0.2,color='black',alpha=1) +
-  scale_colour_manual(values= mycolor2) +#??
-  scale_fill_manual(values= mycolor1) +#??
-  scale_size_continuous( range = c(0.5,6) ) +
-  expand_limits(x = c(-1.3, 1.3), y = c(-1.3, 1.3))+
-  theme_void() +
-  theme(
-    legend.position="none",
-    plot.margin=unit(c(0,0,0,0),"cm"))+
-  coord_fixed(ratio=1)
-
-ggsave('imp_circle_r1_Genome.pdf',width=19.2,height = 5.4)
 
 # importance plot
 load(file='rflistGenome.rda')
@@ -272,9 +182,9 @@ miplot<-list()
                               y_measure = "node_purity_increase",
                               size_measure = "p_value", no_of_labels = 5)+
       theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank())+
-      theme(axis.line=element_line(color='black'),#坐标线颜色
-            axis.ticks.length=unit(0.5,'line'))+#坐标线短线
-      coord_fixed(ratio=1)+#坐标轴y/x比例
+      theme(axis.line=element_line(color='black'),
+            axis.ticks.length=unit(0.5,'line'))
+      coord_fixed(ratio=1)+
       theme(legend.position=c(0.1,0.8))
   })
   ggsave(paste0('m_im_',label[1],'.pdf'),width=5,height=5)
@@ -305,10 +215,10 @@ fiplot<-list()
   interactions_frame<-inter_list[[1]]
   as.vector(interactions_frame$Occurrences)
   as.vector(inter_frame$occurrences)
-  hlim<-ceiling(max(interactions_frame[1:50,3],interactions_frame[1:50,6]))#
-  fip<-plot_min_depth_interactions(interactions_frame,k=50)+#选取互动性最高的50个
+  hlim<-ceiling(max(interactions_frame[1:50,3],interactions_frame[1:50,6]))
+  fip<-plot_min_depth_interactions(interactions_frame,k=50)+
   scale_y_continuous(limits=c(0,hlim+1.5),expand=c(0,0))+
-  scale_fill_gradient(low='#0099f7', high='#f11712')+#渐变色
+  scale_fill_gradient(low='#0099f7', high='#f11712')+
   theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank())+
   theme(legend.position="bottom",legend.box="horizontal")+
   scale_y_discrete(limits=c("minimum","unconditional","occurrances"))
@@ -320,7 +230,7 @@ save(fiplot,file='inter_plotGenome.rda')
 #----------------------------------------------
 # Feature Interaction Calculate
 #----------------------------------------------
-setwd('~/Desktop/Genome')
+setwd('~/')
 source('min_depth_distribution.R')
 source('measure_importance.R')
 source('min_depth_interactions.R')
@@ -328,46 +238,6 @@ source('interaction.R')
 rdlist<-list()
 r_interaction<-list()
 
-pb <- tkProgressBar("Title","Label", 0, 100) 
-for (i in 1:length(label)){
-  print(paste0(i,'/',length(label)))
-  info<- sprintf("?????? %d%%", round(i*100/length(label))) 
-  rf<-rf.list[[i]]
-  c1<-rep(1:length(colindex),each=length(colindex))
-  c11<-colindex[c1]
-  c2<-rep(1:length(colindex),length(colindex))
-  c22<-colindex[c2]
-  rd_frame<-data.frame(c11,c22)
-  colnames(rd_frame)=c('variable','root_variable')
-  im_frame<-mi[[i]]
-  rd_frame<-merge(rd_frame,im_frame[c(1,6)],all.x=T)
-  
-  pb1 <- tkProgressBar("Title","Label", 0, 100) 
-  for (j in 1:rf$ntree){
-    info1<- sprintf("?????? %d%%", round(j*100/rf$ntree)) 
-    D<-calculate_max_depth(rf,j)
-    interactions_frame_single<-min_depth_interactions_single(rf,j,colindex)
-    rD<-calculate_rD(D,interactions_frame_single,j)
-    rD<-cbind(interactions_frame_single[1:2],rD)
-    rd_frame<-merge(rd_frame,rD,by=c('variable','root_variable'),all=T)
-    setTkProgressBar(pb1, j*100/rf$ntree, sprintf("???? (%s)", info1),info1) 
-  }
-  close(pb1)
-  
-  rd_frame[is.na(rd_frame)]<-0
-  rdlist[[label[i]]]<-rd_frame
-  for (k in 1:nrow(rd_frame)){
-    rd_frame[k,504]<-sum(rd_frame[k,4:503])/rd_frame[k,3]
-  }
-  r_frame<-rd_frame[c(1,2,504)]
-  colnames(r_frame)<-c("variable" , "root_variable" ,"r")
-  r_interaction[[label[i]]]<-r_frame
-  #save(rd_frame, file = paste0("Rda/rd_frame_",label[i],".rda"))
-  #save(r_frame,file = paste0("Rda/r_frame_",label[i],".rda"))
-  setTkProgressBar(pb, i*100/length(label), sprintf("?ܽ??? (%s)", info),info) 
-}
-close(pb)
-save(r_interaction,file='r-interactionGenome.rda')
 
 #----------------------------------------------
 # Node and Edge files
@@ -386,8 +256,7 @@ for (i in 1:length(label)){
   nodes<-data.frame(id=c(1:length(colindex)),label=c(colindex[-69],label[i]))
   nodes<-merge(nodes,type,all.x=T)
   nodes<-arrange(nodes,nodes['id'])
-  #write.csv(nodes,paste0('network/nodes_',label[i],'.csv'),row.names=FALSE,fileEncoding='UTF-8')
- 
+
   r_frame<-r_interaction[[i]]
   edges<-cbind(r_frame,c(rep('x-x',nrow(r_frame))))
   colnames(edges)<-c('Source','Target','Weight','Type')
@@ -435,8 +304,6 @@ train<-read.xlsx('ss-index-all_Genome_hpo.xlsx',1)$X0
     data_train<-data[train,]
     data_test<-data[-train,]
     seed<-rep(1,12)
-    #seed[8]<-2
-    #seed[12]<-8
     set.seed(seed[1])
     rf<-randomForest(Toxicity_Conc~.,data=data_train,importance=TRUE,proximity=T,ntree=600,mtry=9)
     
@@ -447,7 +314,7 @@ write.xlsx(interactions_frame,"interaction_frame.xlsx")
   k=280
   subpdp<-list()
   subpdpplot<-list()
-  while (j<=2){                             #选取前x个最重要的特征
+  while (j<=2){                           
     interpair<-inter_frame$interaction[k]
     v1<-strsplit(interpair,':')[[1]][1]
     v2<-strsplit(interpair,':')[[1]][2]
@@ -508,17 +375,7 @@ save(pdpplot,file='pdpplotGenome.rda')
   ggsave(plot=part1,file=paste0('',label[1],'-1.pdf'),width=13,height=8)
   ggsave(plot=part3,file=paste0('',label[1],'-2.pdf'),width=13,height=7.7)
 
-
-# layout <- '
-# AAABBBB
-# AAABBBB
-# AAABBBB
-# CCCDDEE
-# CCCDDEE
-# CCCFFGG
-# CCCFFGG
-
-setwd("~/Desktop/Genome")
+setwd("~/")
 datalist<-list()
       data<-read.xlsx('Standard_Genome.xlsx',1,header=TRUE)
       colnames(data)<-colindex
@@ -569,8 +426,6 @@ for (i in 1:9){
 
 netpdprda<-netpdpplot[[1]]+netpdpplot[[2]]+netpdpplot[[3]]+
   netpdpplot[[4]]
-#+netpdpplot[[5]]+netpdpplot[[6]]+
-#  netpdpplot[[7]]+netpdpplot[[8]]+netpdpplot[[9]]
  plot_layout(ncol=3,widths=rep(3,9),height=rep(3.9))
 save(netpdpplot,file='netpdp.rda')
 ggsave(file='figure-pdp.pdf',width=9,height=9)
@@ -579,7 +434,7 @@ ggsave(file='figure-pdp.pdf',width=9,height=9)
 #####################################################################
 #####                    permutation test                       #####
 #####################################################################
-setwd("~/Desktop/Genome")
+setwd("~/")
 q2plot<-list()
 
 
@@ -643,13 +498,8 @@ write.xlsx(cormat,file = 'cormat-Genome.xlsx',col.names = T,row.names = T)
 #####################################################################
 filelist<-dir(pattern = '.R')
 for (ifile in filelist){
-  source(paste0("~/Desktop/TBRFA/yfuber-TBRFA-d77db1a",ifile))
+  source(paste0("~/",ifile))
 }
-source('D:/?ű??ļ?/tabplot-master/build/createpalette.R')
-source('D:/?ű??ļ?/bit_4.0.3/R/????????.R')
-options(fftempdir = 'D:/fftemp')
-#setwd('??your path??/code')
-setwd("~/Desktop/TBRFA/yfuber-TBRFA-d77db1a")
 library(xlsx)
 #library(tabplot)
 library(RColorBrewer)
@@ -661,22 +511,12 @@ colindex<-c('MW','diameter','surface','Chemical_Purity','size','zp','Dissolution
             'Moxygen','Xox','x','sigma_x','sigma_xnO','Toxicity_Conc')
 colnames(data)<-(colindex)
 feature<-data[1:19]
-#feature$Shape<-as.character(feature$Shape)
 col<-colnames(feature)
-#feature$Shape[which(feature$Shape=='0')]<-'0-Particles'
-#feature$Shape[which(feature$Shape=='1')]<-'1-D'
-#feature$Shape[which(feature$Shape=='2')]<-'2-D'
-#feature$Animal[which(feature$Animal=='Mouse')]<-'Mice'
-#color_enps<-c(read.xlsx('??????ɫ.xlsx',1,header=FALSE)$X1)
-#col_enps<-c()
 f_color<-brewer.pal(12,"Paired")
 pal<-colorRampPalette(f_color)
 f_color<-pal(19)
-#for (i in 1:6){
-#col_enps<-c(col_enps,color_enps[c(seq(i,60,by=6))])
-#}
 set.seed(2)
-setwd("~/Desktop/TBRFA/yfuber-TBRFA-d77db1a")
+setwd("~/")
 tabplot::tableplot(feature,decreasing =FALSE,
                    nBins=100,
                    select=c(MW,diameter_mean,surface,Chemical_Purity,size_mean,zp_mean,Dissolution,
@@ -687,14 +527,6 @@ tabplot::tableplot(feature,decreasing =FALSE,
                              Functionalization=f_color))         
 
 ggsave(filename = 'tab-imm684.pdf',width=25.2,height = 5.4)
-#feature$Weight<-log10(feature$Weight)
-#tabplot::tableplot(feature, decreasing=F,
-                   #select=c(Types,Animal,Gender,Weight,Age),
-                   #pals=list(Types=brewer.pal(8,'Set3')))
-
-#tabplot::tableplot(feature,sortCol = Types,decreasing=F,
-                   #select=c(Types,Method,Duration,Frequency,Recovery,Dose),
-                   #pals=list(Types=brewer.pal(8,'Set3')))
 
 dev.new()
 set.seed(2)
@@ -716,7 +548,7 @@ ggsave(filename = 'tab-imm.pdf',width=19.2,height = 25.4)
 #####################################################################
 #####                    Feature shuffle                        #####
 #####################################################################
-setwd("~/Desktop/Genome")
+setwd("~/")
 datalist<-list()
 
     data<-read.xlsx('Standard_Genome.xlsx',1,header=TRUE)
